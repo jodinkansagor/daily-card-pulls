54d52a63fc3380d787ddc210000350a75fbafe6a
Jodi N Kansagor <joedvice@gmail.com>
2019-12-20 14:23:58 -0800

    auth signup method and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User')
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,19 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can sign up a user with email and password', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ username: 'jbj', email: 'jbj@jbj.com', password: 'password' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: 'jbj',
+          email: 'jbj@jbj.com',
+          __v: 0
+        });
+      });
   });
 });
