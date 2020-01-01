af3b66e95081be01da56eb6375e01fa8b936b9f9
Jodi N Kansagor <joedvice@gmail.com>
2020-01-01 14:15:57 -0800

    added delete button with confirmation pop up

--- public/CardList/CardItem.js
+++ public/CardList/CardItem.js
@@ -1,13 +1,38 @@
 import Component from '../Component.js';
 
 class CardItem extends Component {
+
+  onRender(dom) {
+    const cardPull = this.props.cardPull;
+    console.log(cardPull._id);
+    const deleteButton = dom.querySelector('button[id="delete"]');
+    // const editButton = dom.querySelector('button[id="edit"]');
+
+
+    deleteButton.addEventListener('click', () => {
+      if (confirm('Are you sure you want to delete?')) {
+        fetch(`api/v1/cardPulls/${cardPull._id}`, {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(cardPull)
+        })
+          .then(res => res.json())
+          .then(() => {
+            window.location.reload();
+          });
+      } else {
+        window.location.reload();
+      }
+    });
+  }
   renderHTML() {
     const cardPull = this.props.cardPull;
 
     let cardPullCategory = cardPull.category.reduce((acc, curr) => {
       return acc += `<span>${curr} </span>`;
     }, '');
-    console.log(cardPull);
     
     let cardPullDate = new Date(cardPull.date).toISOString().split('T')[0];
 
@@ -18,6 +43,10 @@ class CardItem extends Component {
         <h2>Deck: ${cardPull.deck}</h2>
         <p>${cardPullCategory.toUpperCase()}</p>
         <p class="explanation">${cardPull.explanation}</p>
+        <div class="buttons">
+          <button id="edit">Edit</button>
+          <button id="delete">Delete</button>
+        </div>
       </li>
     `;
   }
