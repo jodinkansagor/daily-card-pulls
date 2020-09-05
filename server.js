e67683fa0d11ba6cd72aa90b946574468f5768bc
jodinkansagor <joedvice@gmail.com>
2020-09-05 13:14:08 -0700

    add same site none to cooke to help with chromes new cookie rules

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-require('./lib/utils/connect')();
+require("dotenv").config();
+require("./lib/utils/connect")();
 
-const app = require('./lib/app');
+const app = require("./lib/app");
 
 const PORT = process.env.PORT || 7890;
 
